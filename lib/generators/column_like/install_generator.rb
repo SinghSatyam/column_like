bef28c0f4d612090826db348b645b9b42769442d
Satyam Singh <satyam.mgs@gmail.com>
2015-05-03 23:09:47 +0530

    Removing Pry

--- lib/generators/column_like/install_generator.rb
+++ lib/generators/column_like/install_generator.rb
@@ -1,10 +1,8 @@
 require 'rails/generators'
 require 'rails/generators/base'
-require 'pry'
 module ColumnLike
   module Generators
     class InstallGenerator < Rails::Generators::Base
-    	binding.pry
       source_root File.expand_path("../templates", __FILE__)
       desc "Creates initializers for ColumnLike"
   	  def copy_initializer
